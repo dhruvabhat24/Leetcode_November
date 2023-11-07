326d1baf88de812aef27626b8fb0714b3675711e
Dhruva Bhat S N <dhruvabhat24@gmail.com>
2023-11-07 10:11:40 +0530

    Create 7.js

---
+++ 7.js
@@ -0,0 +1,18 @@
+var eliminateMaximum = function(dist, speed) {
+    const n = dist.length;
+    for (let i = 0; i < dist.length; i++) {
+        dist[i] = Math.ceil(dist[i] / speed[i]);
+        speed[i] = 0;
+    }
+    for (const num of dist) {
+        if (num >= n) continue;
+        speed[num] += 1;
+    }
+    for (let i = 1; i < speed.length; i++) {
+        speed[i] += speed[i-1];
+        if (speed[i] > i) {
+            return i;
+        }
+    }
+    return n;
+};
