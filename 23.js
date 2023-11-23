8c0953ef5f12d60215825259050901d5ebccada8
Dhruva Bhat S N <dhruvabhat24@gmail.com>
2023-11-23 06:21:21 +0530

    Create 23.js

---
+++ 23.js
@@ -0,0 +1,45 @@
+/**
+ * @param {number[]} nums
+ * @param {number[]} l
+ * @param {number[]} r
+ * @return {boolean[]}
+ */
+var checkArithmeticSubarrays = function(nums, l, r) {
+    let result = [];
+    for (let i = 0; i < l.length; i++) {
+        result.push(isArithmetic(nums.slice(l[i], r[i] + 1)));
+    }
+    return result;
+};
+
+function isArithmetic(arr) {
+    let min = Math.min(...arr);
+    let max = Math.max(...arr);
+
+    if (min == max) {
+        return true;
+    }
+
+    let step = (max - min) / (arr.length - 1);
+
+    if (step != Math.floor(step)) {
+        return false;
+    }
+
+    let set = new Set();
+    for (let x of arr) {
+        if (set.has(x)) {
+            return false;
+        }
+
+        set.add(x);
+    }
+
+    for (let x = min; x <= max; x += step) {
+        if (!set.has(x)) {
+            return false;
+        }
+    }
+
+    return true;
+}
